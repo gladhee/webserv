bde3ef7c41da573b9bc1128ce23d41e0499ab7ea
gladhee <gladhee@icloud.com>
2025-03-03 16:07:29 +0900

    feat EventLoop : add main loop for kqueue

---
+++ include/event/EventLoop.hpp
@@ -0,0 +1,40 @@
+//
+// Created by HeeSung Choi on 3/3/25.
+//
+
+#ifndef WEBSERV_EVENTLOOP_HPP
+#define WEBSERV_EVENTLOOP_HPP
+
+#include <fcntl.h>
+#include <sys/event.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "Connection.hpp"
+
+typedef struct kevent Kevent;
+
+class EventLoop {
+private:
+    static int const MAX_EVENTS = 32;
+    int _kq;
+    std::vector<Kevent> _changeList;
+    std::map<int, Connection*> _connections;
+
+public:
+    EventLoop();
+
+    ~EventLoop();
+
+    void run();
+
+private:
+    EventLoop(const EventLoop&);
+
+    EventLoop& operator=(const EventLoop&);
+};
+
+#endif  // WEBSERV_EVENTLOOP_HPP
---
+++ src/event/EventLoop.cpp
@@ -0,0 +1,72 @@
+//
+// Created by HeeSung Choi on 3/3/25.
+//
+
+#include "event/EventLoop.hpp"
+
+EventLoop::EventLoop() {
+    _kq = kqueue();
+    if ( _kq == -1 ) {
+        perror("kqueue");
+        exit(EXIT_FAILURE);
+    }
+}
+
+EventLoop::EventLoop(const EventLoop&) {
+}
+
+EventLoop& EventLoop::operator=(const EventLoop&) {
+    return *this;
+}
+
+EventLoop::~EventLoop() {
+    close(_kq);
+}
+
+void EventLoop::run() {
+    Kevent eventList[MAX_EVENTS];
+
+    while ( true ) {
+        int nev =
+            kevent(_kq, &_changeList[0], static_cast<int>(_changeList.size()), eventList, 8, NULL);
+
+        if ( nev < 0 ) {
+            std::cerr << "kevent error" << std::endl;
+            continue;
+        }
+
+        for ( int i = 0; i < nev; ++i ) {
+            Kevent& curr = eventList[i];
+            int fd = static_cast<int>(curr.ident);
+
+            if ( _connections.find(fd) == _connections.end() ) {
+                continue;
+            }
+
+            Connection* conn = _connections[fd];
+
+            if ( curr.flags & EV_ERROR ) {
+                int errCode = static_cast<int>(curr.data);
+                std::cerr << errCode << "EV_ERROR" << std::endl;
+                conn->onError();
+                delete conn;
+                _connections.erase(fd);
+                continue;
+            }
+
+            if ( curr.flags & EV_EOF ) {
+                std::cerr << "EV_EOF" << std::endl;
+                conn->onClose();
+                _connections.erase(fd);
+                continue;
+            }
+
+            if ( curr.filter == EVFILT_READ ) {
+                conn->onRead();
+            }
+            if ( curr.filter == EVFILT_WRITE ) {
+                conn->onWrite();
+            }
+        }
+    }
+}
