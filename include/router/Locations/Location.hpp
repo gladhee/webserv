59b34b5270b9b02588af2663a37d264de262809e
heechoi <heechoi@student.42seoul.kr>
2025-03-04 21:40:40 +0900

    feat Location : add Location interface for Cgi and Static Location

---
+++ include/router/Locations/Location.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Heesung Choi on 3/4/25.
+//
+
+#ifndef WEBSERV_LOCATION_HPP
+#define WEBSERV_LOCATION_HPP
+
+#include <string>
+
+class Location {
+
+public:
+    virtual bool canHandleRequest(std::string const& uri) const = 0;
+    virtual int handleRequest(std::string const& uri) const = 0;
+
+};
+
+#endif  // WEBSERV_LOCATION_HPP
