7980d653888e7069d94123249d48a1e7b629f328
heechoi <heechoi@student.42seoul.kr>
2025-03-04 21:49:30 +0900

    feat Method : manage a method by enum

---
+++ include/router/Locations/Method.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Heesung Choi on 3/4/25.
+//
+
+#ifndef WEBSERV_METHOD_HPP
+#define WEBSERV_METHOD_HPP
+
+enum Method {
+    GET,
+    POST,
+    DELETE,
+};
+
+#endif  // WEBSERV_METHOD_HPP
